8fb0c24f21a01201e4e590f9d93f6feb4c61f3f1
Neeraj9991 <118429028+Neeraj9991@users.noreply.github.com>
2023-06-12 21:45:22 +0530

    added some filters
    more options to add filter on the image

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from flask import Flask, render_template, request, flash
 from werkzeug.utils import secure_filename
 import os
@@ -13,6 +14,38 @@ app.secret_key = 'the random string'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
+# Apply grayscale effect
+def apply_grayscale(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Apply sepia effect
+def apply_sepia(img):
+    kernel = np.array([[0.272, 0.534, 0.131],
+                       [0.349, 0.686, 0.168],
+                       [0.393, 0.769, 0.189]])
+    sepia_image = cv2.transform(img, kernel)
+    return sepia_image
+
+# Apply blur filter
+def apply_blur(img):
+    blurred_image = cv2.GaussianBlur(img, (15, 15), 0)
+    return blurred_image
+
+# Apply edge detection filter
+def apply_edge_detection(img):
+    edges = cv2.Canny(img, 100, 200)
+    return edges
+
+# Apply custom filter
+def apply_custom_filter(img):
+    # Define the custom filter kernel
+    kernel = np.array([[0, -1, 0],
+                       [-1, 5, -1],
+                       [0, -1, 0]])
+    filtered_image = cv2.filter2D(img, -1, kernel)
+    return filtered_image
+
+
 def processImage(filename, operation):
     print(f'The filename is {filename}')
     print(f'The operation is {operation}')
@@ -21,19 +54,31 @@ def processImage(filename, operation):
 
     match operation:
         case "cgray":
-            processed_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            processed_image = apply_grayscale(img)
             newFilename = f"static/{filename}"
             cv2.imwrite(newFilename, processed_image)
             return newFilename
-        case "cjpg":
-            newFilename = f"static/{filename.split('.')[0]}.jpg"
-            cv2.imwrite(newFilename, img)
+        case "csepia":
+            processed_image = apply_sepia(img)
+            newFilename = f"static/{filename.split('.')[0]}_sepia.jpg"
+            cv2.imwrite(newFilename, processed_image)
             return newFilename
-        case "cpng":
-            newFilename = f"static/{filename.split('.')[0]}.png"
-            cv2.imwrite(newFilename, img)
+        case "cblur":
+            processed_image = apply_blur(img)
+            newFilename = f"static/{filename.split('.')[0]}_blur.jpg"
+            cv2.imwrite(newFilename, processed_image)
             return newFilename
-    pass
+        case "cedge":
+            processed_image = apply_edge_detection(img)
+            newFilename = f"static/{filename.split('.')[0]}_edge.jpg"
+            cv2.imwrite(newFilename, processed_image)
+            return newFilename
+        case "ccustom":
+            processed_image = apply_custom_filter(img)
+            newFilename = f"static/{filename.split('.')[0]}_custom.jpg"
+            cv2.imwrite(newFilename, processed_image)
+            return newFilename
+
 
 
 def allowed_file(filename):
